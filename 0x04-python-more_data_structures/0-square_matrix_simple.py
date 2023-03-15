433923321a12d49fe5bf52ae3de529961d54631b
Revelation-coder <captainrevelation@gmail.com>
2023-03-15 09:38:34 +0000

    search and replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-	squared_matrix = matrix(map(lambda x: x**2, matrix))
+	squared_matrix = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
 	return squared_matrix
