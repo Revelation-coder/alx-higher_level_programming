c8804d932315edb8ee2a0067a32890d431e5d174
Revelation-coder <captainrevelation@gmail.com>
2023-03-28 13:10:09 +0000

    pycode and documenting

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
+"""Module containing Square class"""
 
 
 class Square:
-    '''Initialize Class Square
-    '''
+    '''Class representing a square shape'''
 
     def __init__(self, size=0):
-        '''init method of class Square
+        '''Initializes a new Square object with a specified size.
+
+        Args:
+            size (int): The size of the square.
         '''
         self.size = size
 
     @property
     def size(self):
+        '''Getter method for size attribute.'''
         return self.__size
 
     @size.setter
     def size(self, size):
+        '''Setter method for size attribute.
+
+        Args:
+            size (int): The size of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        '''
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -23,9 +35,11 @@ class Square:
         self.__size = size
 
     def area(self):
+        '''Returns the area of the square.'''
         return (self.size * self.size)
 
     def my_print(self):
+        '''Prints the square to the console.'''
         if self.__size == 0:
             print()
             return
