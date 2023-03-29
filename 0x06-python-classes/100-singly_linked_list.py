06cfb87513f7bfcc6a0e4b788191ab76e3639277
Revelation-coder <82636655+Revelation-coder@users.noreply.github.com>
2023-03-29 10:02:50 +0200

    documenting

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,33 +1,45 @@
 #!/usr/bin/python3
+'''this module defines class Node and class SinglyLinkedList'''
 class Node:
+    '''initialises node class'''
     def __init__(self, data, next_node=None):
+        '''Initializes node'''
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        '''function to return data'''
         return self._data
 
     @data.setter
     def data(self, value):
+        '''setter function for node
+         Args: value
+        '''
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self._data = value
 
     @property
     def next_node(self):
+        '''Function to get next node'''
         return self._next_node
 
     @next_node.setter
     def next_node(self, value):
+        '''function to set next node'''
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self._next_node = value
 class SinglyLinkedList:
+    '''class do define a singlylinkedlist'''
     def __init__(self):
+        '''function to initialise list'''
         self.head = None
 
     def __repr__(self):
+        '''function to initialize header'''
         node = self.head
         nodes = []
         while node is not None:
@@ -36,6 +48,7 @@ class SinglyLinkedList:
         return "\n".join(nodes)
 
     def sorted_insert(self, value):
+        '''function to sort list'''
         new_node = Node(value)
         if self.head is None or value <= self.head.data:
             new_node.next_node = self.head
--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+'''This module defines square  class'''
 class Square:
+    '''Initialize a square class'''
     def __init__(self, size=0):
         self.size = size
 
     @property
     def size(self):
+        '''getter function'''
         return self._size
 
     @size.setter
     def size(self, value):
+        '''Setter function '''
         if not isinstance(value, (int, float)):
             raise TypeError("size must be a number")
         if value < 0:
@@ -16,23 +20,30 @@ class Square:
         self._size = value
 
     def area(self):
+        '''function to get area'''
         return self.size ** 2
 
     def __eq__(self, other):
+        '''Function to compare area'''
         return self.area() == other.area()
 
     def __ne__(self, other):
+        '''Function to compare area'''
         return self.area() != other.area()
 
     def __gt__(self, other):
+        '''Function to compare area'''
         return self.area() > other.area()
 
     def __ge__(self, other):
+        '''Function to compare area'''
         return self.area() >= other.area()
 
     def __lt__(self, other):
+        '''Function to compare area'''
         return self.area() < other.area()
 
     def __le__(self, other):
-        return self.area() <= other.area()
+       '''Function to compare area'''
+       return self.area() <= other.area()
 
