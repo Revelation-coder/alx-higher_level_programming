09aaaa730e3077a3a0a03b5eb82676c97012cc1c
Revelation-coder <captainrevelation@gmail.com>
2023-03-29 08:30:01 +0000

    pycode

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -59,8 +59,8 @@ class SinglyLinkedList:
             self.head = new_node
         else:
             current_node = self.head
-            while current_node.next_node is not None \
-               and current_node.next_node.data < value:
+            while (current_node.next_node is not None and
+                   current_node.next_node.data < value):
                 current_node = current_node.next_node
             new_node.next_node = current_node.next_node
             current_node.next_node = new_node
