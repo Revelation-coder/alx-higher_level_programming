623ebf382520b307e8d9fcfe506e7aec1d99f67f
Revelation-coder <captainrevelation@gmail.com>
2023-04-03 05:36:04 -0700

    rectangle and permissions

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,7 +8,6 @@ class Rectangle:
 """
 A rectangle class with private attributes width and height.
 """
-
 def __init__(self, width=0, height=0):
     """
     Initializes a rectangle object with width and height.
@@ -53,3 +52,18 @@ def height(self, value):
         raise ValueError("height must be >= 0")
     else:
         self.__height = value
+
+def area(self):
+    """
+    Returns the area of the rectangle.
+    """
+    return self.__width * self.__height
+
+def perimeter(self):
+    """
+    Returns the perimeter of the rectangle.
+    """
+    if self.__width == 0 or self.__height == 0:
+        return 0
+    return 2 * (self.__width + self.__height)
+
