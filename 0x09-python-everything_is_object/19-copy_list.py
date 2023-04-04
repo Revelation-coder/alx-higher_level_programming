814a54dbcf31cac24118917609e12d4ba0857d3d
Revelation-coder <captainrevelation@gmail.com>
2023-04-04 05:58:53 -0700

    fix

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def copy_list(l):
-    return l[:]
+def copy_list(original_list):
+    """
+    This function creates a copy of a given list and returns it.
+    """
+    return list(original_list)
