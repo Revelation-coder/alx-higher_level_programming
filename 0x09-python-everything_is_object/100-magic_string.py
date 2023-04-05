c1b55e4db8a55f38bea6582f38e79bdcb54fa963
Revelation-coder <captainrevelation@gmail.com>
2023-04-05 06:49:53 -0700

    line length fix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-''' This module defines a function which returns n times string'''
-
-
 def magic_string():
-    '''magic string function no arguments'''
     magic_string.counter = getattr(magic_string, "counter", 0) + 1
     return ", ".join(["Holberton" for i in range(magic_string.counter)])
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+Module for locked class
+"""
 class LockedClass:
+    '''
+    class for lockedclass
+    '''
     __slots__ = "first_name"
