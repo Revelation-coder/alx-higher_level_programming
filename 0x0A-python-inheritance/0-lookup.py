7d4804287e25f7b898566ce456606ff1fbf8c532
Revelation-coder <82636655+Revelation-coder@users.noreply.github.com>
2023-04-11 15:29:29 +0200

    finishing up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
+''' function that returns the list
+'''
 
 
 def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object.
-    
-    Args:
-        obj: The object to inspect.
-  
-    Returns:
-    A list of strings representing the available attributes and methods of the object.
-    """
-    return [attr for attr in dir(obj) 
-    if not callable(getattr(obj, attr)) 
-    or not attr.startswith("__")]
+    ''' function: lookup()
+    Returns a list object
+    '''
+    return dir(obj)
