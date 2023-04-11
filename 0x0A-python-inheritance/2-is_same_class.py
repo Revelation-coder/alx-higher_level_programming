8720c4c655b1d4f72de813140035e1f5c87ab153
Revelation-coder <captainrevelation@gmail.com>
2023-04-11 06:51:17 -0700

    pycodestyle fix

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+'''
+this module defines a function to check whther an object
+is exactly an instance of the specified class
+'''
 
 
 def is_same_class(obj, a_class):
-    """'''function: is_same_class
+    """
+    function: is_same_class
     obj: an object
     a_class: a class
     Returns: Bool
