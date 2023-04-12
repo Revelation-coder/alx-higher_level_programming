05f3427cb163a86d105f81480a7ad91756547340
Revelation-coder <captainrevelation@gmail.com>
2023-04-12 06:52:47 -0700

    added file

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,8 +3,8 @@
 This script reads stdin line by line and computes metrics.
 """
 
-import sys
 
+import sys
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 file_size = 0
 count = 0
