081772343a071ecb320c7342abfda5baddb238fc
Revelation-coder <captainrevelation@gmail.com>
2023-04-12 01:07:36 -0700

    pycodestyle

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-function that appends a string at the end of a text file (UTF8) and returns the number of characters added
+function that appends a string at the end of a text file (UTF8)
+returns the number of characters added
 """
 
 def append_write(filename="", text=""):
+    """Function to append text to a file"""
     with open(filename, 'a') as f:
         return f.write(text)
