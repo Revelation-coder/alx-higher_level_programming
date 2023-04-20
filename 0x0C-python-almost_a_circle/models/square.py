9d96d84bb77f8b6a6b4765d135f206d3c5ae02ae
Revelation-coder <captainrevelation@gmail.com>
2023-04-20 03:02:39 -0700

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -43,3 +43,24 @@ class Square(Rectangle):
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
         return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w", encoding="utf-8") as file:
+            file.write(cls.to_json_string([obj.to_dictionary()
+                                          for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
