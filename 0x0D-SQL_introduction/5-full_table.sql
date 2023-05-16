19739f1edafe2b12093e6e21d2ad7a60dc1e451d
Revelation-coder <captainrevelation@gmail.com>
2023-05-16 04:30:30 -0700

    fix 2

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,11 +1,2 @@
 -- Script: Print the full description of the table first_table
-
--- Set the database name
-SET @database_name = 'hbtn_0c_0';
-
--- Print table description
-SELECT column_name, column_type
-FROM information_schema.columns
-WHERE table_schema = @database_name
-  AND table_name = 'first_table';
-
+SHOW CREATE TABLE first_table
