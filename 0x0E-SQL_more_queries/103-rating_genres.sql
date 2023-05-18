3dbfb897adf4e4b3c70ac1f62431003454acc9bc
Revelation-coder <captainrevelation@gmail.com>
2023-05-18 03:03:07 -0700

    fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,6 @@
 --script  that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT tv_genres.name, SUM(rating) AS rating_sum
+SELECT tv_genres.name, SUM(rating) AS rating
 FROM tv_genres
-JOIN tv_shows ON tv_genres.id = tv_shows.genre_id
+JOIN tv_show_ratings ON tv_genres.id = tv_shows_ratings.show_id
 GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
-
+ORDER BY rating DESC;
