e7727f2ab9088baab773209d4a79dbdeee7d63ab
Revelation-coder <captainrevelation@gmail.com>
2023-06-30 03:21:38 -0700

    fix main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,14 @@ Fetches the status from https://alx-intranet.hbtn.io/status
 
 from urllib import request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with request.urlopen(url) as response:
-    content = response.read()
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
 
-    print("Body response:")
-    print("    - type: {}".format(type(content)))
-    print("    - content: {}".format(content))
-    print("    - utf8 content: {}".format(content.decode('utf-8')))
+    with request.urlopen(url) as response:
+        content = response.read()
+
+        print("Body response:")
+        print("    - type: {}".format(type(content)))
+        print("    - content: {}".format(content))
+        print("    - utf8 content: {}".format(content.decode('utf-8')))
