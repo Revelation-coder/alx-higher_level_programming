6c3be0aaac548a680837395aefc4431044f455a1
Revelation-coder <captainrevelation@gmail.com>
2023-06-30 03:58:07 -0700

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,21 +6,23 @@ Uses the GitHub API with Basic Authentication to display your GitHub ID.
 import requests
 import sys
 
-if len(sys.argv) != 3:
-    sys.exit("Usage: ./10-my_github.py username password")
 
-username = sys.argv[1]
-password = sys.argv[2]
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.exit("Usage: ./10-my_github.py username password")
 
-url = 'https://api.github.com/Revelation-coder'
+    username = sys.argv[1]
+    password = sys.argv[2]
 
-response = requests.get(url, auth=(username, password))
+    url = 'https://api.github.com/Revelation-coder'
 
-try:
-    data = response.json()
-    if 'id' in data:
-        print(data['id'])
-    else:
-        print("None")
-except ValueError:
-    print("Invalid response from GitHub API")
+    response = requests.get(url, auth=(username, password))
+
+    try:
+        data = response.json()
+        if 'id' in data:
+            print(data['id'])
+        else:
+            print("None")
+    except ValueError:
+        print("Invalid response from GitHub API")
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Sends a request to a URL and displays the value of the 
+Sends a request to a URL and displays the value of the
 X-Request-Id variable in the response header.
 """
 
