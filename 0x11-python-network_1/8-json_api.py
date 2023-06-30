5093172dd6d1d2884f5e3056b54e4bedb7d4d537
Revelation-coder <captainrevelation@gmail.com>
2023-06-30 06:53:40 -0700

    done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
     try:
         json_response = response.json()
         if json_response:
-            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+            print("[{}] {}".format(json_response.get('id'),
+                                   json_response.get('name')))
         else:
             print("No result")
     except ValueError:
