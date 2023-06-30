8ae72a99fa0c139f2dfb9596218f5c469f527c87
Revelation-coder <captainrevelation@gmail.com>
2023-06-30 03:43:51 -0700

    fixing pycode and main funcion

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,10 +5,12 @@ Fetches the status from https://alx-intranet.hbtn.io/status using the requests p
 
 import requests
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-response = requests.get(url)
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
 
-print("Body response:")
-print("    - type: {}".format(type(response.text)))
-print("    - content: {}".format(response.text))
+    response = requests.get(url)
+
+    print("Body response:")
+    print("    - type: {}".format(type(response.text)))
+    print("    - content: {}".format(response.text))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
-Sends a POST request to a URL with an email parameter and displays the body of the response.
+Sends a POST request to a URL with an email parameter and 
+Displays the body of the response.
 """
 
 import requests
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
 
-payload = {'email': email}
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-response = requests.post(url, data=payload)
-print("Your email is: {}".format(response.text))
+    payload = {'email': email}
+
+    response = requests.post(url, data=payload)
+    print("Your email is: {}".format(response.text))
