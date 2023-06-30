7b528213be44973f78b1765fb5db7144142cfbe1
Revelation-coder <captainrevelation@gmail.com>
2023-06-30 01:10:28 -0700

    3 lines fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request, and displays the size
-# Send a request to the URL and extract the Content-Length header
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
