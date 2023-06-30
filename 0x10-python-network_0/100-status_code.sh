e0cdc14ee9d0fa4dc3c45aff0997dfad1b8f7554
Revelation-coder <captainrevelation@gmail.com>
2023-06-30 01:29:55 -0700

    advanced tasks

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Script to send request to a URL and display the status code
-
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-echo "$response"
-
+curl -s -o /dev/null -w "%{http_code}" "$1"
