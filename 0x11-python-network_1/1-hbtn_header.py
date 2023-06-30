58ddec437de9500abd1681638ab5eb01a994a95b
Revelation-coder <captainrevelation@gmail.com>
2023-06-30 03:28:37 -0700

    fix main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,10 @@ Sends a request to a URL and displays the value of the X-Request-Id variable in 
 import urllib.request
 import sys
 
-url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    header = response.getheader('X-Request-Id')
-    print(header)
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        header = response.getheader('X-Request-Id')
+        print(header)
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,12 +7,14 @@ import urllib.parse
 import urllib.request
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data=data, method='POST')
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
-    print(body)
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
+
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
